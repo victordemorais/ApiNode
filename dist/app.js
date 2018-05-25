73708b2295e77fdd3357aeb151222833d15df0fc
Victor de Morais <victordemorais@hotmail.com.br>
2018-05-24 23:30:05 -0300

    utils js

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,15 @@
-console.log("Hello asdfsfWorld!");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express = require("express");
+class App {
+    constructor() {
+        this.express = express();
+        this.middleare();
+    }
+    middleare() {
+        this.express.use('/hello', (req, res, next) => {
+            res.send("helloWorld");
+        });
+    }
+}
+exports.default = new App().express;
--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello asdfsfWorld!");
+import * as express from 'express';
+class App {
+    public express: express.Application;
+
+    constructor() {
+        this.express = express();
+        this.middleare();
+    }
+    private middleare(): void {
+        this.express.use('/hello', (req, res, next) => {
+            res.send("helloWorld");
+        })
+    }
+}
+export default new App().express
